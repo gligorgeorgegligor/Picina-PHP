a591ee7c60d50e98836386b832887f6bfdb81a89
Gligor George Gligor <george_gg_90@yahoo.com>
2015-03-19 16:02:17 +0200

    one_more_time.php
    one_more_time.php good version

--- Jour_02/ex01/one_more_time.php
+++ Jour_02/ex01/one_more_time.php
@@ -1,66 +1,30 @@
 #!/usr/bin/php
 <?php
-	function epur($str)
+function match_array($array, $elem)
+{
+	foreach($array as $a)
 	{
-		$arg = trim($str);
-		return (preg_replace('/\s+/', ' ', $str));
+		if (preg_match("/".$a."/", $elem))
+			return (array_search($a, $array));
 	}
-	$days = array("lundi",
-					"mardi",
-					"mercredi",
-					"jeudi",
-					"vendredi",
-					"samedi",
-					"dimanche");
-	$month = array("janvier", "fevrier", "mars", "avril", "mai", "juin",
-					"juillet", "aout", "septembre", "octobre", "novembre",
-					"decembre");
-	function extract_day_id($str)
-	{
-		global $days;
-		for($i = 0; $i < count($days); $i ++)
-		{
-			if ($days[$i] == strtolower($str))
-				return ($i);
-		}
-		return (-1);
-	}
-	function extract_month_id($str)
-	{
-		global $month;
-		for($i = 0; $i < count($month); $i ++)
-		{
-			if ($month[$i] == strtolower($str))
-				return ($i);
-		}
-		return (-1);
-	}
-	function err()
-	{
-		return ("Wrong Format");
-	}
-	function parse_date($str)
-	{
-		global $days;
-		global $month;
-		$dte = explode(' ', epur($str));
-		if (count($dte) != 5)
-			return ("Wrong Format");
-		$wd = extract_day_id($dte[0]);
-		$d = $dte[1];
-		if (strlen($d) != 2)
-			return (err());
-		$m = extract_month_id($dte[2]);
-		$y = $dte[3];
-		if (strlen($y) != 4)
-			return (err());
-		$t = explode (':', epur($dte[4]));
-		if (count($t) != 3)
-			return (err());
-		if (strlen($t[0]) != 2 || strlen($t[1]) != 2 || strlen($t[2]) != 2)
-			return (err());
-		return (mktime($t[0],$t[1],$t[2], $m, $d, $y));
-	}
-	if (isset($argv[1]))
-		echo parse_date($argv[1]), "\n";
+	return (-1);
+}
+if (count($argv) < 2)
+	exit();
+$day = array("[lL]undi", "[mM]ardi", "[mM]ercredi", "[jJ]eudi", "[vV]endredi", "[sS]amedi", "[dD]imanche");
+$month = array("01" => "[jJ]anvier", "02" => "[fF][eé]vrier", "03" => "[mM]ars", "04" => "[aA]vril", "05" => "[mM]ai", "06" => "[jJ]uin", "07" => "[jJ]uillet", "08" => "[aA]oi[uû]t", "09" => "[sS]eptembre", "10" => "[oO]ctobre", "11" => "[nN]ovembre", "12" => "[dD][ée]cembre");
+$days = implode("|", array_values($day));
+$months = implode("|", array_values($month));
+if (preg_match("/^(".$days.") [1-3]?[0-9] (".$months.") [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}$/", $argv[1]) == 1)
+{
+	$split = preg_split("/ /", $argv[1]);
+	$date = $split[3].":".match_array($month, $split[2]).":".$split[1]." ".$split[4];
+	date_default_timezone_set("Europe/Paris");
+	$time = strtotime($date);
+	$verif = date("Y:m:d G:i:s", $time);
+	if (!empty($date) && $verif == $date)
+		echo $time."\n";
+	else
+		echo "Wrong Format\n";
+}
 ?>
